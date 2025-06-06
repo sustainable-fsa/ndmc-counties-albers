2ddaa13c06456469da9a1735956ae3dc835f760c
bocinsky <bocinsky@gmail.com>
2025-06-06 13:13:34 -0600

    push readme

--- ndmc-counties-albers.R
+++ ndmc-counties-albers.R
@@ -1,20 +1,4 @@
-# install.packages("pak")
-# pak::pak(
-#   c("magrittr",
-#     "tidyverse",
-#     "sf",
-#     "tigris",
-#     "rmapshaper")
-# )
-
-library(magrittr)
-library(tidyverse)
-library(sf)
-library(rmapshaper)
-
-dir.create("topojson",
-           showWarnings = FALSE,
-           recursive = TRUE)
+install.packages("curl")
 
 ## Download the NDMC Albers.gdb.zip file
 curl::multi_download("https://droughtcenter.unl.edu/Outgoing/Albers.gdb.zip",
